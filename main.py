37b410dfe0f74288a579e52ee2746371ffa57c7a
lucascaovilla <lucasgrisac@gmail.com>
2023-04-18 17:53:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print('hello world')
+l = [1,2,3,4,5,6,7,8,9,10]
+print(list(map(lambda x: x**2, l)))
+
+
+def runb_threaded(job_func):
+    job_thread = threading.Thread(target=job_func)
+    job_thread.start()
