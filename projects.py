0f9f23a123f1f464ef9121baf8092b128f529891
lucas <lucasgrisac@gmail.com>
2023-06-06 15:00:10 -0300

    Adding file responsible for dealing with the repos

---
+++ projects.py
@@ -0,0 +1,5 @@
+# this file must acquire all the data from the repos here:https://github.com/lucascaovilla,
+# organize them to be shown as list and as a detailed description
+# (there must be a running window with the project environment)
+
+# may use requests+bs4 or scrapy
